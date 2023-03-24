f9a8170e88f303e6f11502247c94607ba75ab6a6
OskPaa <osku.paakkonen2@gmail.com>
2023-03-24 13:21:20 +0200

    home commit

---
+++ Datakertaus/Datakertaus/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Datakertaus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kerrotaan käyttäjälle, mitkä muuttujat luodaan ja mitkä niiden arvot ovat.
+            Console.WriteLine("Luodaan muuttujia eri datatyypeillä");
+            
+            int kuukaudet = 12;
+            Console.WriteLine($"{kuukaudet} - ilmaisee kuukausien määrän vuodessa!");
+            
+            char valuutta = '€';
+            Console.WriteLine($"{valuutta} - ilmaisee Euroopassa käytettävän rahayksikön!");
+            
+            double pii = 3.14;
+            Console.WriteLine($"{pii} - ilmaisee piin arvon kahden desimaalin tarkkuudella!");
+
+            string marja = "mustikka";
+            Console.WriteLine($"{marja} - ilmaisee  tumman marjan nimen!");
+
+            //Kerrotaan käyttäjälle, että muuttujien arvot muutetaan.
+            Console.WriteLine("Annetaan muuttujille eri arvot");
+
+            kuukaudet = 6;
+            Console.WriteLine($"{kuukaudet} - ilmaisee kuukausien määrän puolessa vuodessa!");
+
+            valuutta = '$';
+            Console.WriteLine($"{valuutta} - ilmaisee Amerikassa käytettävän rahayksikön!");
+
+            pii = 3.14159;
+            Console.WriteLine($"{pii} - ilmaisee piin arvon viiden desimaalin tarkkuudella!");
+
+            marja = "mansikka";
+            Console.WriteLine($"{marja} - ilmaisee punaisen marjan nimen!");
+        }
+    }
+}
---
+++ pinta_ala/pinta_ala/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pinta_ala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna korkeus metreinä:");
+            int korkeus = Int32.Parse(Console.ReadLine());
+            Console.Write("Anna leveys metreinä:");
+            int leveys = Int32.Parse(Console.ReadLine());
+            int pintaAla = korkeus * leveys;
+            Console.WriteLine($"Antamillasi mitoilla pinta-ala on {pintaAla} neliömetriä.");
+        }
+    }
+}
