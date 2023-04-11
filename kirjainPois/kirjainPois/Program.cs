b54ed55c75a45ff48d016fc0b70551a23b65ec97
OskPaa <osku.paakkonen2@gmail.com>
2023-04-11 19:49:54 +0300

    April 11th commit

---
+++ kirjainPois/kirjainPois/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kirjainPois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna merkkijono: "); // Kysytään merkkijonoa
+            string input = Console.ReadLine(); // Käyttäjä antaa merkkijonon
+            Console.Write("Montako merkkiä poistetaan jonon alusta ja lopusta: "); // Kysytään poistettavien kirjainten määrää
+            int removal = int.Parse(Console.ReadLine()); // Käyttäjä antaa poistettavien kirjainten lukumäärän
+
+            while (removal <= 0 || removal*2 >= input.Length) // Poistomäärän tarkistus
+            {
+                Console.WriteLine("Syöttämäsi luku ei saa olla nollaa pienempi tai liian suuri!"); // Virheilmoitus
+                Console.Write("Montako merkkiä poistetaan jonon alusta ja lopusta: "); // Kysytään uusiksi
+                removal = int.Parse(Console.ReadLine()); // Luetaan uusi syöte
+            }
+
+            int leftover = input.Length - (removal*2); // Lasketaan montako merkkiä kirjoitetaan Substring metodilla
+
+            Console.WriteLine(input.Substring(removal,leftover)); // Tulostetaan ohjelman tuotos
+
+            // Suljetaan ohjelma
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close program");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ merkkiTulostus/merkkiTulostus/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace merkkiTulostus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Syötä sana ja tulostus suunta (\"Lattia A\"): ");
+            string word = Console.ReadLine();
+            Console.Write("Montako merkkiä sanasta tulostetaan: ");
+            int loops = int.Parse(Console.ReadLine());
+            string[] wordSplit = word.Split(' ');
+
+            if (wordSplit[1]=="A")
+            {
+
+            }
+            else if (wordSplit[1]=="L")
+            {
+                char[] chars = wordSplit[0].ToCharArray();
+                Array.Reverse(chars);
+                string reverseWord = new string(chars);
+                wordSplit[0] = reverseWord;
+            }
+                foreach(char mark in wordSplit[0])
+                {
+                    Console.WriteLine(mark);
+                }
+            
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close program");
+            Console.ReadKey();
+        }
+    }
+}
