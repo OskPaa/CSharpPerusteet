a4f04fc2f98ecdb77765bb92ece9ed8e7a0dea5e
OskPaa <osku.paakkonen2@gmail.com>
2023-05-02 19:59:53 +0300

    may 2nd, 2023 commit

---
+++ secondSymbol/secondSymbol/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secondSymbol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static int SecondSymbol(string str, char symbol)
+        {
+            int foundInstances = 0;
+
+            for (int i = 0; i > str.Length; i++)
+            {
+                if (str[i] == symbol)
+                {
+                    if (foundInstances < 1)
+                    {
+                        foundInstances++;
+                    }
+                    else
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
