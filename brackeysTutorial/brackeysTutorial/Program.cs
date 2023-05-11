a5592473bf79713eabe2c908b8761e45d5d1bd1f
OskPaa <osku.paakkonen2@gmail.com>
2023-05-11 18:24:17 +0300

    may 11th, 2023 commit

---
+++ brackeysTutorial/brackeysTutorial/Program.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brackeysTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // #1 Basics
+            
+            // Muutetaan ulkoasua
+            Console.Title = "Skynet";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+
+            // Keskustelua konsolin ja käyttäjän välillä
+            Console.WriteLine("Hello, what's your name?");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("My name is RX-9000.");
+            Console.Write("I am an AI sent from the future to ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("DESTROY MANKIND.");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("What is your favorite color?");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write("Cool! Mine is ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("DESTRUCTION.");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // #2 Variables
+
+            Console.WriteLine("\nWhat is your name?");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {userName}, nice to meet you!");
+
+            double num01;
+            double num02;
+
+            Console.Write("Input a number: ");
+            num01 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Input a second number: ");
+            num02 = Convert.ToDouble(Console.ReadLine());
+
+            double result = num01 * num02;
+            Console.WriteLine($"The result is {result}");
+
+            // #3 Conditions
+
+            Console.WriteLine("\nWelcome! Tickets are 5$. Please insert cash.");
+
+            int cash = Convert.ToInt32(Console.ReadLine());
+
+            if(cash < 5)
+            {
+                Console.WriteLine("That's not enough money.");
+            }
+            else if(cash == 5)
+            {
+                Console.WriteLine("Here is your ticket.");
+            }
+            else
+            {
+                int change = cash - 5;
+                Console.WriteLine($"Here is your ticket and {change} dollars in change.");
+            }
+
+            int age;
+            int height;
+
+            Console.Write("\nPlease input your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please input your height (cm): ");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            if(age >= 18 || height >= 160)
+            {
+                Console.WriteLine("You can enter!");
+            }
+            else
+            {
+                Console.WriteLine("You don't meet the requirements.");
+            }
+
+            Console.Write("\nInput a number between 1 and 5: ");
+            int num = Convert.ToInt32(Console.ReadLine()); ;
+
+            switch (num)
+            {
+                case 1: Console.WriteLine("One");break;
+                case 2: Console.WriteLine("Two"); break;
+                case 3: Console.WriteLine("Three"); break;
+                case 4: Console.WriteLine("Four"); break;
+                case 5: Console.WriteLine("Five"); break;
+                default: Console.WriteLine("Default"); break;
+            }
+
+            // #4 Loops
+
+            Console.Write("\nHow many cool numbers do you want: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= count; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+
+            Random numberGen = new Random();
+
+            int roll = 0;
+            int attempts = 0;
+
+            Console.WriteLine("\nPress enter to roll the die.");
+
+            while (roll != 6)
+            {
+                Console.ReadKey();
+
+                roll = numberGen.Next(1, 7);
+                Console.WriteLine($"\nYou rolled: {roll}");
+                attempts++;
+            }
+
+            Console.WriteLine($"It took you {attempts} attempts to roll a six.");
+
+            // #5 Arrays
+
+            string[] movies = { "Lord of the Rings", "Fight Club", "Interstellar", "Gladiator"};
+            Array.Sort(movies);
+            Console.WriteLine("Here are four movies arranged alpahabetically.");
+            for(int i = 0; i < movies.Length; i++)
+            {
+                int rank = i + 1;
+                Console.WriteLine($"{rank}. {movies[i]}");
+            }
+
+            List<string> shoppingList = new List<string>();
+            shoppingList.Add("Dreams");
+            shoppingList.Add("Miracles");
+            shoppingList.Add("Rainbows");
+            shoppingList.Add("Pony");
+
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                int rank = i + 1;
+                Console.WriteLine($"{shoppingList[i]}");
+            }
+
+            shoppingList.Remove("Dreams");
+            shoppingList.RemoveAt(1);
+
+            Console.WriteLine("---------------------------");
+
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                int rank = i + 1;
+                Console.WriteLine($"{shoppingList[i]}");
+            }
+
+            // #6 Methods
+
+            MeetAlien();
+            Console.WriteLine("---------------------------");
+            MeetAlien();
+
+            int multiply = Multiply(3, 8);
+
+            Console.WriteLine($"The result is {multiply}");
+
+            if (multiply % 2 == 0)
+            {
+                Console.WriteLine($"{multiply} is an even number.");
+            }
+            else
+            {
+                Console.WriteLine($"{multiply} is an uneven number.");
+            }
+
+            // #7 Classes
+
+            Wizard wizard01 = new Wizard("Parry Hopper", "Unexpecto Patronum");
+
+            wizard01.CastSpell();
+
+            Wizard wizard02 = new Wizard("Glindalf Merlinson", "Abracadabra");
+
+            wizard02.CastSpell();
+
+            Console.WriteLine(Wizard.Count);
+
+            // Suljetaan ohjelma
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+        // #6 Methods
+        static void MeetAlien()
+        {
+            Random numGen = new Random();
+
+            string name = $"X-{numGen.Next(10, 9999)}";
+            int age = numGen.Next(10, 500);
+            Console.WriteLine($"My name is{name}.");
+            Console.WriteLine($"I am {age} years old.");
+            Console.WriteLine("Oh, also I'm an alien.");
+        }
+
+        static int Multiply(int num1, int num2) 
+        {
+            return num1 * num2;
+        }
+    }
+    // #7 Classes
+    class Wizard
+    {
+        public string name;
+        public string favoriteSpell;
+        private int spellSlots;
+        private float experience;
+
+        public static int Count;
+
+        public Wizard(string _name, string _favoriteSpell)
+        {
+            name = _name;
+            favoriteSpell = _favoriteSpell;
+            spellSlots = 2;
+            experience = 0f;
+            
+            Count++;
+        }
+
+        public void CastSpell()
+        {
+            if(spellSlots > 0)
+            {
+                Console.WriteLine($"{name} casts {favoriteSpell}");
+                spellSlots--;
+                experience += 0.3f;
+            }
+            else
+            {
+                Console.WriteLine($"{name} is out of spell slots");
+            }
+        }
+        public void Meditate()
+        {
+            Console.WriteLine($"{name} meditates to regain spell slots");
+            spellSlots = 2;
+        }
+    }
+}
---
+++ dotnetTutorial/dotnetTutorial/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstFriend = "Scott";
+            string secondFriend = "Kendra";
+            Console.WriteLine($"Hello {firstFriend} and {secondFriend}");
+
+            Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+            Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+            string greeting = "        Hello World!        ";
+            Console.WriteLine($"[{greeting}]");
+            
+            string trimmedGreeting = greeting.TrimStart();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.TrimEnd();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            string sayHello = "Hello World!";
+            Console.WriteLine(sayHello);
+
+            sayHello = sayHello.Replace("World", "Universe");
+            Console.WriteLine(sayHello);
+
+            Console.WriteLine(sayHello.ToUpper());
+            Console.WriteLine(sayHello.ToLower());
+
+            string songLyrics = "You say greetings, and I say hello";
+
+            bool result = songLyrics.Contains("You");
+
+            Console.WriteLine(result);
+            Console.WriteLine(songLyrics.Contains("greetings"));
+
+            /*
+            int a = 18;
+            int b = 6;
+            int c = a / b;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+
+            Console.WriteLine($"The range of integer is {min} to {max}");
+
+            int what = max + 3;
+
+            Console.WriteLine($"An example of overflow: {what}");
+
+            double a = 5;
+            double b = 4;
+            double c = 2;
+            double d = (a + b) / c;
+
+            Console.WriteLine(d);
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+
+            Console.WriteLine($"The range of double is {min} to {max}");
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+
+            decimal max = decimal.MaxValue;
+            decimal min = decimal.MinValue;
+
+            Console.WriteLine($"The range of decimal is {min} to {max}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+            */
+
+            double radius = 2.50;
+            double area = Math.PI * radius * radius;
+
+            Console.WriteLine(area);
+
+            /*
+            int a = 5;
+            int b = 4;
+
+            if (a + b > 10)
+            {
+                Console.WriteLine("The anwer is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("The anwer is not greater than 10");
+            }
+            */
+
+            int a = 5;
+            int b = 3;
+            int c = 4;
+
+            if (a + b + c > 10 && a==b)
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+            }
+
+            int counter = 10;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            } while (counter < 10);
+
+            for (int index = 0; index < 10; index++)
+            {
+                Console.WriteLine($"Hello World! The index is {index}");
+            }
+
+
+            int sum = 0;
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 3== 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"The sum is {sum}");
+
+            List<string> names = new List<string> { "<name>", "Ana", "Felipe" };
+
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Felipe");
+
+            names.Sort();
+
+            foreach (string name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            int index2 = names.IndexOf("Bill");
+
+            if(index2 == -1)
+            {
+                Console.WriteLine($"When an item is not found IndexOf returns {index2}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index2]} is at index {index2}");
+            }
+
+            List<int> fibonaciNumbers = new List<int> { 1, 1 };
+
+
+            while(fibonaciNumbers.Count < 20)
+            {
+                int previous = fibonaciNumbers[fibonaciNumbers.Count - 1];
+                int previous2 = fibonaciNumbers[fibonaciNumbers.Count - 2];
+                fibonaciNumbers.Add(previous + previous2);
+            }
+
+
+            foreach(int item in fibonaciNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            BankAccount account = new BankAccount("Kendra", 10000);
+
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            account.MakeWithDrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+
+
+            account.MakeWithDrawal(50, DateTime.Now, "Xbox Game");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+            
+            //try
+            //{
+            //    account.MakeWithDrawal(75000, DateTime.Now, "Attempt to overdraw");
+            //}
+            //catch(InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+            
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch(ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
