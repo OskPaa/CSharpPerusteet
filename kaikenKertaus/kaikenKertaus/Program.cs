c7cbf34de9b58fefb9037dd9a66602880f9732b5
OskPaa <osku.paakkonen2@gmail.com>
2023-05-15 14:59:56 +0300

    home commit may 15th, 2023

--- kaikenKertaus/kaikenKertaus/Program.cs
+++ kaikenKertaus/kaikenKertaus/Program.cs
@@ -13,46 +13,85 @@ namespace kaikenKertaus
             var animals = new List<Animal>(); // Luodaan Animal-luokan objekteille lista
             string contLoop = "";
             
-            do //Lisätään loopissa eläimiä
+            do // Lisätään loopissa ainakin  1 eläin
             {
-                Console.Write("Anna eläimen laji: ");
+                Console.Write("Anna eläimen laji: "); // Kysytään eläimen laji käyttäjältä
                 string species = Console.ReadLine();
 
-                Console.Write("Anna eläimen nimi: ");
+                Console.Write("Anna eläimen nimi: "); // Kysytään eläimen nimi käyttäjältä
                 string name = Console.ReadLine();
 
-                Console.Write("Anna eläimen sukupuoli(F/M): ");
+                Console.Write("Anna eläimen sukupuoli(F/M): "); // Kysytään eläimen sukupuoli käyttäjältä
                 string genderString = Console.ReadLine();
                 char gender = genderString[0];
 
-                Console.Write("Anna eläimen paino(kg): ");
+                Console.Write("Anna eläimen paino(kg): "); // Kysytään eläimen paino käyttäjältä
                 double weight = double.Parse(Console.ReadLine());
 
-                Console.Write("Anna eläimen ikä: ");
+                Console.Write("Anna eläimen ikä: "); // Kysytään eläimen ikä käyttäjältä
                 int age = int.Parse(Console.ReadLine());
 
-                // Lisätään listaan Animal-luokan objekti
+                // Luodaan ja lisätään listaan Animal-luokan objekti käyttäjän antamilla tiedoilla 
                 animals.Add(new Animal(species,name,gender,weight,age));
 
-                Console.Write("Lisätäänkö vielä eläimiä?(k jatkaa): ");
-                contLoop = Console.ReadLine();
+                Console.Write("Lisätäänkö vielä eläimiä?(k jatkaa): "); // Kysytään, jatketaanko eläinten lisäämistä
+                contLoop = Console.ReadLine(); // Käyttäjä päättää/vastaa, jatketaanko eläinten lisäämistä
 
-            } while (contLoop.Contains("k"));
+            } while (contLoop.Contains("k")); // Tarkistetaan loopin lopussa, jatkuuko eläimien lisäys
 
-            Console.WriteLine("Laji\tNimi\tSukupuoli\tPaino\tIkä\tRokotettu");
+            Console.WriteLine("Laji\tNimi\tSukupuoli\tPaino\tIkä\tRokotettu"); // Kirjoitetaan otsikot tulostettaville tiedoille
             foreach(var item in animals)
             {
-                Console.WriteLine($"{item.Species}\t{item.Name}\t{item.Gender}\t\t{item.Weight}\t{item.Age}\t{item.Vaccinated}");
+                // Tulostetaan listaan tallennetun objektin tiedot omalle rivilleen
+                Console.WriteLine($"{item.Species}\t{item.Name}\t{item.Gender}\t\t{item.Weight}\t{item.Age}\t{item.Vaccinated}"); 
             }
 
+            // Tallenetaan rokotetut eläimet taulukkoon
+            Animal[] vaxAnimals = GetHealthyAnimals(animals);
+
+            if (vaxAnimals.Length > 0) // Katsotaan onko taulukossa yhtään rokotettua eläintä 
+            {
+                foreach(var item in vaxAnimals)
+                {
+                    Console.WriteLine($"{item.Name} on rokotettu."); // Tulostettaan, minkä niminen eläinj on rokotettu
+                }            
+            }
+            else 
+            {
+                // Tulostetaan ilmoitus, jossa kerrotaan ettei rokotettuja eläimiä ole
+                Console.WriteLine("Taulukko ei sisällä yhtään rokotettua eläintä.");  
+            }
+
+            // Suljetaan ohjelma
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
 
-        public Animal[] GetHealthyAnimals()
+        public static Animal[] GetHealthyAnimals(List<Animal> animals)
         {
-            Animal[] result = new Animal[0];
-            return result;
+            int vaxCount = 0;
+            
+            foreach(var item in animals) // Lasketaan loopissa rokotetut eläimet listalta
+            {
+                if (item.Vaccinated)
+                {
+                    vaxCount++;
+                }
+            }
+
+            Animal[] result = new Animal[vaxCount]; // Luodaan sopivan kokoinen lista palautettavaksi
+            int arrayIndex = 0; // Luodaan laskuri taulukon indeksointia varten
+
+            foreach (var item in animals) // Taulukoidaan rokotettut eläimet loopissa
+            {
+                if (item.Vaccinated)
+                {
+                    result[arrayIndex] = item;
+                    arrayIndex++;
+                }
+            }
+
+            return result; // Palautetaan taulukko
         }
     }
 }
