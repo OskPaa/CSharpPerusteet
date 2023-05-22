a5fc065068a0c2a92515452236130533bbf178e5
OskPaa <osku.paakkonen2@gmail.com>
2023-05-22 14:12:43 +0300

    home commit may 22nd, 2023

---
+++ pitsanTilaus/pitsanTilaus/Pizza.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pitsanTilaus
+{
+    class Pizza
+    {
+       // Properties
+        public List<Topping> Toppings { get; set; }
+        public decimal BasePrice { get; set; }
+        // Constructor
+        public Pizza()
+        {
+            Toppings = new List<Topping>();
+            BasePrice = 15M;
+        }
+        // Methods
+        public decimal CalculatePrice()
+        {
+            if (Toppings.Count < 4)
+            {
+                return BasePrice;
+            }
+            else
+            {
+                return BasePrice + (Toppings.Count - 3);
+            }
+        }
+    }
+}
